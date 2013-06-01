a62b8dce350dc8184561484c554c5ada58b25841
agustinusakava <agustinusakava@0e8fcaa2-a0c4-19ef-2c0b-e6d71cfd3030>
2013-06-01 04:13:45 +0000

    find broo...

--- trunk/BDBO_REKAMMEDIS/src/Controller/ControlData.java
+++ trunk/BDBO_REKAMMEDIS/src/Controller/ControlData.java
@@ -9,6 +9,9 @@ import Data.Pasien;
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
+import com.db4o.query.Predicate;
+import com.db4o.query.Query;
+import java.awt.RenderingHints.Key;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,11 +23,20 @@ import java.util.List;
 public class ControlData {
 
     public static ObjectContainer db;
+    private static final String DataBase = "RekamMedisData.yap";
+
+    private static void resetDatabase() {
+        new File(DataBase).delete();
+    }
+
+    public static void bukaDatabase() {
+        db = Db4o.openFile(DataBase);
+    }
 
     public static void input(Object data) {
-        db = Db4o.openFile("RekamMedisData.yap");
+        db = Db4o.openFile(DataBase);
         db.set(data);
-
+        db.close();
     }
 
     public static void listResult(ObjectSet res) {
@@ -32,19 +44,36 @@ public class ControlData {
             System.out.println(res.next());
         }
     }
+
     public static Obat[] Result(ObjectSet res) {
         Obat obt[] = null;
         while (res.hasNext()) {
-           obt=(Obat[])(res.next());
+            obt = (Obat[]) (res.next());
         }
-        return  obt;
+        return obt;
     }
 
-    public static List<Pasien> ResultPasien(ObjectSet res) {
-        List<Pasien> barang = new ArrayList<Pasien>();
+    public static List<Pasien> ResultPasien() {
+        List<Pasien> pas = new ArrayList<Pasien>();
+        bukaDatabase();
+        Query query = db.query();
+        query.constrain(Pasien.class);
+        ObjectSet res = query.execute();
         while (res.hasNext()) {
-          
+            pas.add((Pasien) res.next());
         }
-        return  barang;
+        return pas;
+    }
+
+    public static List<Pasien> ResultPasienSelected(final String key) {
+        List<Pasien> pas = new ArrayList<Pasien>();
+        Query query = db.query();
+        query.constrain(Pasien.class);
+        query.descend("id").constrain(key).like();
+        ObjectSet res = query.execute();
+        while (res.hasNext()) {
+            pas.add((Pasien) res.next());
+        }
+        return pas;
     }
 }
